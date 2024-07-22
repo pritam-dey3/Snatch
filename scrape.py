88dd50922d1d2c2f875c567af4fecff50850fda3
Pritam Dey <pdeypritam@gmail.com>
2024-07-22 16:09:16 +0530

    rename html_dir to save_dir and pass more options to get_driver

--- scrape.py
+++ scrape.py
@@ -6,11 +6,11 @@ import traceback as tb
 import uuid
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from driver import get_driver
+from driver import get_driver, SystemType
 from utils import get_id, start_xvfb, stop_xvfb
 
 SYS = "amd64"
-HTML_DIR = Path("/scrape/html_files/")
+SAVE_DIR = Path("/scrape/html_files/")
 GECKO_PATH = Path("/snatch/tor-browser/")
 THREAD_FAIL_LIMIT = 20
 
@@ -26,11 +26,11 @@ THREAD_LOCAL.n_failures = 0
 
 
 class Driver:
-    def __init__(self, system: str = ""):
+    def __init__(self, system: SystemType = "", save_dir: str = ""):
         self.id = uuid.uuid4()
         self.display = start_xvfb()
-        self.driver = get_driver(system=system)
-        self.driver.set_page_load_timeout(15)
+        self.driver = get_driver(system=system, save_dir=save_dir)
+        # self.driver.set_page_load_timeout(15)
         logging.info(f"Created driver {self.id}.")
 
     def __del__(self):
@@ -39,10 +39,12 @@ class Driver:
         logging.info(f"The driver {self.id} has been quitted.")
 
     @classmethod
-    def create_driver(cls):
+    def create_driver(cls, system: SystemType = "", save_dir: str | Path = ""):
+        if isinstance(save_dir, Path):
+            save_dir = save_dir.as_posix()
         the_driver = getattr(THREAD_LOCAL, "the_driver", None)
         if the_driver is None:
-            the_driver = cls()
+            the_driver = cls(system, save_dir)
             THREAD_LOCAL.the_driver = the_driver
         driver = the_driver.driver
         the_driver = None
@@ -62,7 +64,7 @@ def scraper(url):
         driver.get(url)
         url_id = get_id(url)
 
-        file = HTML_DIR / f"{url_id}.html"
+        file = SAVE_DIR / f"{url_id}.html"
         with open(file, "w") as f:
             f.write(driver.page_source)
     except Exception as e:
@@ -86,13 +88,13 @@ def scrape_urls(
     thread_fail_limit: int | None = None,
     n_threads: int | None = None,
 ):
-    global SYS, HTML_DIR, GECKO_PATH, THREAD_FAIL_LIMIT
+    global SYS, SAVE_DIR, GECKO_PATH, THREAD_FAIL_LIMIT
     SYS = system if system else SYS
-    HTML_DIR = html_dir if html_dir else HTML_DIR
+    SAVE_DIR = html_dir if html_dir else SAVE_DIR
     GECKO_PATH = gecko_path if gecko_path else GECKO_PATH
     THREAD_FAIL_LIMIT = thread_fail_limit if thread_fail_limit else THREAD_FAIL_LIMIT
 
-    logging.info(f"Using {SYS=}, {THREAD_FAIL_LIMIT=}, {HTML_DIR=}, {GECKO_PATH=}")
+    logging.info(f"Using {SYS=}, {THREAD_FAIL_LIMIT=}, {SAVE_DIR=}, {GECKO_PATH=}")
 
     with ThreadPoolExecutor(max_workers=n_threads) as executor:
         futures = [executor.submit(scraper, url) for url in urls]
